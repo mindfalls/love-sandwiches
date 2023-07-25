ad4e51e77054bdb2c2937c6457998c6647a83b8d
Mattias Lundkvist <mindfalls@hotmail.com>
2023-07-25 10:42:02 +0000

    add sales data to sales worksheet

--- run.py
+++ run.py
@@ -9,8 +9,8 @@ SCOPE = [
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-GSPRED_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPRED_CLIENT.open('love_sandwiches')
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 def get_sales_data():
     """
@@ -49,4 +49,15 @@ def validate_data(values):
 
     return True
 
+def update_sales_worksheet(data):
+    """
+    Update sales worksheet, add new row with the list provided.
+    """
+    print("Updating sales worksheet...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updated successfully.\n")
+
 data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
